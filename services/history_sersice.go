bdb332fc0c412a16d96fd38fcf1f9f4feae38629
maks <miakishevvv@gmail.com>
2025-03-26 11:49:40 +0300

    testtime

--- services/history_sersice.go
+++ services/history_sersice.go
@@ -197,7 +197,7 @@ func (s *PlaceService) AudioGenerate(text string) ([]byte, error) {
 	}
 
 	// Создаем контекст с тайм-аутом
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	// Создаем HTTP-запрос
